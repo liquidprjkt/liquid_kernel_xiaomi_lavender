cc218b0d8b7514a4d20b04e367ac4cdcbce34b53
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-24 07:22:55 -0700

    simple_lmk: Introduce Simple Low Memory Killer for Android
    This is a complete low memory killer solution for Android that is small
    and simple. Processes are killed according to the priorities that
    Android gives them, so that the least important processes are always
    killed first. Processes are killed until memory deficits are satisfied,
    as observed from kswapd struggling to free up pages. Simple LMK stops
    killing processes when kswapd finally goes back to sleep.
    
    The only tunables are the desired amount of memory to be freed per
    reclaim event and desired frequency of reclaim events. Simple LMK tries
    to free at least the desired amount of memory per reclaim and waits
    until all of its victims' memory is freed before proceeding to kill more
    processes.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: UsiF <xprjkts@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -78,7 +78,7 @@
 #include <linux/compiler.h>
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
-#include <linux/cpufreq_times.h>
+#include <linux/simple_lmk.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -229,7 +229,6 @@ static void account_kernel_stack(unsigned long *stack, int account)
 
 void free_task(struct task_struct *tsk)
 {
-	cpufreq_task_times_exit(tsk);
 	account_kernel_stack(tsk->stack, -1);
 	arch_release_thread_stack(tsk->stack);
 	free_thread_stack(tsk->stack);
@@ -725,6 +724,7 @@ static inline void __mmput(struct mm_struct *mm)
 	}
 	if (mm->binfmt)
 		module_put(mm->binfmt->module);
+	simple_lmk_mm_freed(mm);
 	mmdrop(mm);
 }
 
@@ -1366,8 +1366,6 @@ static struct task_struct *copy_process(unsigned long clone_flags,
 	if (!p)
 		goto fork_out;
 
-	cpufreq_task_times_init(p);
-
 	/*
 	 * This _must_ happen before we call free_task(), i.e. before we jump
 	 * to any of the bad_fork_* labels. This is to avoid freeing
@@ -1809,8 +1807,6 @@ long _do_fork(unsigned long clone_flags,
 	if (!IS_ERR(p)) {
 		struct completion vfork;
 		struct pid *pid;
-
-		cpufreq_task_times_alloc(p);
 
 		trace_sched_process_fork(current, p);
 
