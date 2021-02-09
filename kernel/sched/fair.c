4dff54cdb8354d2b6f7f1e3be20b82a7ef9e61fe
darkhz <kmachanwenw@gmail.com>
2021-02-10 02:01:18 +0530

    sched/fair: Switch sched scaling to linear
    * This has performance improvements.
    
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: UsiF <xprjkts@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -65,7 +65,7 @@ unsigned int sysctl_sched_cstate_aware = 1;
  * SCHED_TUNABLESCALING_LINEAR - scaled linear, *ncpus
  */
 enum sched_tunable_scaling sysctl_sched_tunable_scaling
-	= SCHED_TUNABLESCALING_LOG;
+	= SCHED_TUNABLESCALING_LINEAR;
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
